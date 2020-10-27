95d334d7f44c76ef9866cb87a8a58d6ce4b31881
firmanjs <fimzhakim@gmail.com>
2020-10-27 16:19:08 +0700

    change docker and crud operations

---
+++ controllers/customer/validation.js
@@ -0,0 +1,10 @@
+'use strict'
+const { check } = require('express-validator')
+require('dotenv').config()
+
+const customerValidation = [
+  check('name').isAlphanumeric().not().isEmpty().withMessage('required value'),
+  check('nip').isNumeric().not().isEmpty().withMessage('required value')
+]
+
+module.exports = { customerValidation }
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { notFoundHandler, errorHandler } = require('./helpers/exceptions')
 const compress = require('compression')
 const methodOverride = require('method-override')
 const helmet = require('helmet')
+// const logger = require('./config/logger')
 require('dotenv').config()
 
 app.use(compress()) // gzip compression
@@ -15,10 +16,11 @@ app.use(methodOverride()) // lets you use HTTP verbs
 app.use(helmet()) // secure apps by setting various HTTP headers
 app.use(cors()) // enable cors
 dbConfig.connectWithRetry() // connect to mongodb
-app.use(express.json())
+app.use(express.json({ limit: '200kb' }))
 app.use(routing) // routing
 app.use(notFoundHandler) // 404 handler
 app.use(errorHandler) // error handler
+// app.use(logger) // error handler
 
 app.listen(process.env.APP_PORT, () => {
   console.log(`express boillerplate app running in port ${process.env.APP_PORT}`)
