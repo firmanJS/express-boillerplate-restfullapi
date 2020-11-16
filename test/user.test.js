8e12634e4a067a033d5db4c1dd6db9974aa661f7
firmanjs <fimzhakim@gmail.com>
2020-11-16 18:49:36 +0700

    change unit testing

--- test/user.test.js
+++ test/user.test.js
@@ -14,22 +14,26 @@ const users = {
   password: '123'
 }
 
+const methodSame = (users, code, done) => {
+  chai.request(server).post('auth/login')
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send(users).then((res) => {
+      res.status.should.be.equal(code)
+      res.body.should.be.a('object')
+      done()
+    }).catch(function (err) {
+      throw err
+    })
+}
+
 // Our parent block
 describe('Users', () => {
   describe('/POST Login user', () => {
     it('it should login user valid user', (done) => {
       assert.typeOf(users.username, 'string')
       assert.typeOf(users.password, 'string')
-      chai.request(server).post('auth/login')
-        .set('Accept', 'application/json')
-        .set('Content-Type', 'application/json')
-        .send(users).then((res) => {
-          res.status.should.be.equal(200)
-          res.body.should.be.a('object')
-          done()
-        }).catch(function (err) {
-          throw err
-        })
+      methodSame(users, 200, done)
     })
   })
 
@@ -39,16 +43,7 @@ describe('Users', () => {
       users.password = '123'
       assert.typeOf(users.username, 'string')
       assert.typeOf(users.password, 'string')
-      chai.request(server).post('auth/login')
-        .set('Accept', 'application/json')
-        .set('Content-Type', 'application/json')
-        .send(users).then((res) => {
-          res.status.should.be.equal(401)
-          res.body.should.be.a('object')
-          done()
-        }).catch(function (err) {
-          throw err
-        })
+      methodSame(users, 401, done)
     })
   })
 })
