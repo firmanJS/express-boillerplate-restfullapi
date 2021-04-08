6fe7d99431cee568d86bfbbddc5e3c0507806716
firmanjs <fimzhakim@gmail.com>
2021-04-08 16:29:20 +0700

    update testing

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testEnvironmentOptions: {
-    NODE_ENV: 'test',
-  },
-  restoreMocks: true,
-  coveragePathIgnorePatterns: ['node_modules', 'config', 'app.js', 'test'],
-  coverageReporters: ['text', 'lcov', 'clover', 'html'],
-}
--- test/user.test.js
+++ test/user.test.js
@@ -1,13 +1,14 @@
-const request = require('supertest')
-const app = require('../app')
+const request = require('supertest');
+const app = require('../app');
 
 // eslint-disable-next-line no-undef
-describe('GET /users', () => {
+describe('api/v1/user ', () => {
   // eslint-disable-next-line no-undef
-  it('respond with json containing a list of all users', async (done) => {
-    const res = await request(app).get('/api/v1/user')
-    // eslint-disable-next-line no-undef
-    expect(res.statusCode).toEqual(401)
-    done()
+  it('not found page', (done) => {
+    request(app)
+      .get('/users')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done);
   });
 });
