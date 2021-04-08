9a420e101f70a707c2f98583f95c6df49ab5ac5a
firmanjs <fimzhakim@gmail.com>
2021-04-08 15:51:23 +0700

    supertest example

--- test/user.test.js
+++ test/user.test.js
@@ -1,13 +1,13 @@
-//apiTest.js
-const request = require('supertest');
-const app = require('../app');
+const request = require('supertest')
+const app = require('../app')
 
-describe('GET /users', function () {
-  it('respond with json containing a list of all users', function (done) {
-    request(app)
-      .get('/users')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(404, done);
+// eslint-disable-next-line no-undef
+describe('GET /users', () => {
+  // eslint-disable-next-line no-undef
+  it('respond with json containing a list of all users', async (done) => {
+    const res = await request(app).get('/api/v1/user')
+    // eslint-disable-next-line no-undef
+    expect(res.statusCode).toEqual(401)
+    done()
   });
 });
